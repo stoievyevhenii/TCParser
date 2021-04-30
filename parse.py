35c901613b0dfc913ddace9e1b1aaa09d6ff659a
Yevhenii Stoiev <stoiev.yevhenii@outlook.com>
2021-04-30 10:54:08 +0300

    Refactored code

--- parse.py
+++ parse.py
@@ -3,29 +3,38 @@ from telethon import TelegramClient, events, sync
 
 # VARIABLES
 channels = []
-api_id = 5220734
-api_hash = 'c66c23afec32a8cfc9919ff9152434e6'
 
-# CONNECTION
-client = TelegramClient('session_name', api_id, api_hash)
-client.start()
+def InitConnection():
+    global client
+    api_id = 5220734
+    api_hash = 'c66c23afec32a8cfc9919ff9152434e6'
+    
+    client = TelegramClient('session_name', api_id, api_hash)
+    client.start()
 
-# GET ALL DIALOGS AND CHATS
-allChannels = client.iter_dialogs()
-print("--- DIALOGS ---")
-for channel in allChannels:
-    channels.append(channel.name)
+def GetData():
+    allChannels = client.iter_dialogs()
+    print("--- DIALOGS ---")
+    for channel in allChannels:
+        channels.append(channel.name)
 
-#CHECK KEYWORD IN CHATS
-print("--- MESSAGES ---")
-for channel in channels:
-    for message in client.get_messages(channel, limit=20):
-        if " " in message.message: 
-            if "art" in message.message:
-                print(message.message)
-                print("Contains")
+def CheckKey():
+    print("--- MESSAGES ---")
+    for channel in channels:
+        for message in client.get_messages(channel, limit=20):
+            if " " in message.message: 
+                if "art" in message.message:
+                    print(message.message)
+                    print("Contains")
+                else:
+                    print(message.message)
+                    print("Don`t contains")
             else:
-                print(message.message)
-                print("Don`t contains")
-        else:
-            continue
+                continue
+
+#
+# Let`s make magic
+#
+InitConnection()
+GetData()
+CheckKey()
