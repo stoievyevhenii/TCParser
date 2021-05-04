4841ed7f761f1148a307b541158d834c87c51266
Yevhenii Stoiev <stoiev.yevhenii@outlook.com>
2021-05-04 17:00:15 +0300

    All params set in file

--- parse.py
+++ parse.py
@@ -8,52 +8,30 @@ except:
     from telethon import TelegramClient, events, sync
 
 # VARIABLES
-channels = []
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+channels = {}
 keywords = {}
+waitTime = 10
 s = sched.scheduler(time.time, time.sleep)
 
-
 # FUNCTIONS
-def InitParams():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--keys', nargs='+',type=str, help="List of keywords")
-    parser.add_argument('--wait', nargs='+',type=int, help="Wait time")
-    args = parser.parse_args()
-
-    keysList = args.keys
-    for key in keysList:
-        keywords['#' + key] = 0
-
-    global waitTime
-    waitTime = args.wait[0]
-
-def ClearConsole():
-    clear = lambda: os.system('cls')
-    clear()
-
-def InitConnection():
-    global client
-    api_id = 5220734
-    api_hash = 'c66c23afec32a8cfc9919ff9152434e6'
-    
-    client = TelegramClient('session_name', api_id, api_hash)
-    client.start()
-
-def GetData():
-    allChannels = client.iter_dialogs()
-    
-    for channel in allChannels:
-        channels.append(channel.name)
-
 def CheckKey():
-    print("--- MESSAGES ---")
     for channel in channels:
+        print(bcolors.OKGREEN + "CHANNEL - " + channel + bcolors.ENDC)
         for message in client.get_messages(channel, limit=20):
             try:
                 for key in keywords:
                     if key in message.message:
-                        print(message.message)
-                        print("Contains")
                         keywords[key] += 1
                     else:
                         continue
@@ -63,21 +41,51 @@ def CheckKey():
 def ShowScanResult():
     print(keywords)
 
+def ResetKeysCounter():
+    keywords.update((key, 0) for key in keywords)
+
+def InitConnection():
+    global client
+    api_id = 5220734
+    api_hash = 'c66c23afec32a8cfc9919ff9152434e6'
+    
+    client = TelegramClient('session_name', api_id, api_hash)
+    client.start()
+
+def SetParam(paramForInit, destDictionary):
+    paramFile = open(paramForInit, 'r')
+    lines = paramFile.readlines()
+    for item in lines:
+        item = item.replace('\n','')
+        destDictionary[item] = 0
+
+def InitParams():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--keys',type=str, help="List of keywords")
+    parser.add_argument('--channels', type=str, help="List of channels")
+    args = parser.parse_args()
+
+    SetParam(paramForInit = args.keys, destDictionary = keywords)
+    SetParam(paramForInit = args.channels, destDictionary = channels)
+
+def ClearConsole():
+    clear = lambda: os.system('cls')
+    clear()
 #
 # LET`S MAKE SOME MAGIC
 #
 
 def StartScriptLogic(sc):
-    ClearConsole()
-    InitConnection()
-    GetData()
     CheckKey()
-    ShowScanResult() #TODO: replace print to email send function
+    ShowScanResult()
+    ResetKeysCounter()
 
     s.enter(waitTime, 1, StartScriptLogic, (sc,))
 
 def main():
+    ClearConsole()
     InitParams()
+    InitConnection()
 
     s.enter(waitTime, 1, StartScriptLogic, (s,))
     s.run()
